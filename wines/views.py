d8e29207e17e718e9748b35c20d60a46835da446
JA <joshappeldev@gmail.com>
2019-05-16 17:51:04 -0400

    deploy prep

--- wines/views.py
+++ wines/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from wines.models import Wine
 from wines.serializers import WineSerializer
 from rest_framework import generics
@@ -6,3 +7,8 @@ from rest_framework import generics
 class WineListCreate(generics.ListCreateAPIView):
     queryset = Wine.objects.all()
     serializer_class = WineSerializer
+
+
+def index(request):
+    wines = Wine.objects.all()
+    return render(request, 'index.html', {'wines': wines})
