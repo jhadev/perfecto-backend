2ef7fe3f3162ccf7475566b465bbd3b7d6f002ac
JA <joshappeldev@gmail.com>
2019-05-17 10:36:40 -0400

    test again

--- perfecto/settings.py
+++ perfecto/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'perfecto.CorsMiddleware'
+    'wines.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'perfecto.urls'
---
+++ wines/cors.py
@@ -0,0 +1,7 @@
+# allow cors
+
+
+class CorsMiddleware(object):
+    def process_response(self, req, resp):
+        response["Access-Control-Allow-Origin"] = "*"
+        return response
