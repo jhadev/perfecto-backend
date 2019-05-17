e3b2408516856bb152de32a96642bbf98c6c05b2
JA <joshappeldev@gmail.com>
2019-05-17 18:50:10 -0400

    add method for bottle price api field

--- wines/serializers.py
+++ wines/serializers.py
@@ -3,6 +3,14 @@ from wines.models import Wine
 
 
 class WineSerializer(serializers.ModelSerializer):
+
+    bottle_price = serializers.SerializerMethodField()
+
     class Meta:
         model = Wine
         fields = '__all__'
+
+    def get_bottle_price(self, obj):
+        total = obj.case_price / obj.case_size
+        total = round(total, 2)
+        return total
